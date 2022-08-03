3d10e07e7af8479ae577693e879d42660f961ab4
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-08-03 23:41:49 +0600

    revission variable conditionals and array

---
+++ Practice-variable-condition-array/variable/01-practice.js
@@ -0,0 +1,7 @@
+// shorthand
+
+var price1 = 45;
+
+price1 /= 7;
+price1 = price1.toFixed(2);
+console.log(price1);
---
+++ Practice-variable-condition-array/variable/02-increment-decre.js
@@ -0,0 +1,12 @@
+
+var debet = 23;
+
+debet++;
+console.log(debet);
+
+
+
+var credit = 23;
+
+credit--;
+console.log(debet);
